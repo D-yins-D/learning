ab3d53435c0c2464ec08dc07bbf513f1cc6c3539
D-yins-D <3261884801@qq.com>
2026-02-02 20:28:48 +0800

    添加海龟画图

--- code/hello.py
+++ code/hello.py
@@ -1,1 +1,18 @@
-print("hello world")
+import turtle
+
+turtle.showturtle()
+turtle.write("hello world")
+turtle.forward(100)
+turtle.color('red')
+turtle.forward(100)
+turtle.left(90)
+turtle.color('green')
+turtle.forward(100)
+turtle.goto(0,0)
+
+turtle.penup()
+turtle.goto(0,100)
+turtle.pendown()
+turtle.circle(50)
+
+turtle.done()   # 保持窗口存在
